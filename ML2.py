d1a5c275ec3b6e2b40f18eed61827718bead9c57
niranjanarayan2128-coder <niranjanarayan2128@gmail.com>
2026-08-27 21:13:12 +0530

    Update ML2.py

--- ML2.py
+++ ML2.py
@@ -23,7 +23,7 @@ st.caption("Here to help, what do you need?")
 
 # --- 2. INITIALIZE BRAIN ---
 groq_api_key = st.secrets["GROQ_API_KEY"]
-llm = ChatGroq(model="openai/gpt-oss-20b", groq_api_key=groq_api_key, temperature=0.4)
+llm = ChatGroq(model="llama3-70b-8192", groq_api_key=groq_api_key, temperature=0.4)
 
 # --- 3. HELPER FUNCTIONS ---
 def quick_search(query):
