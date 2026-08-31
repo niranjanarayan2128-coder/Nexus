e070329425f76066d9f6668db672cf7eca5fcf6c
niranjanarayan2128-coder <niranjanarayan2128@gmail.com>
2026-08-31 10:32:34 +0530

    Update ML2.py

--- ML2.py
+++ ML2.py
@@ -60,7 +60,7 @@ if typed_text:
     system_instruction = (
         "You are an AI assistant named Spark. Assist users with anything. "
         "For news/weather/facts, start with 'SEARCH: '. Otherwise, be like a human assistant "
-        "but dont be brief but dont be a chatter box and if user asks you are made by Niranjan Narayan, a small developer in Kochi, and be funny and kind. "
+        "but be brief and quick and if user asks you are made by Niranjan Narayan, a small developer in Kochi, and be funny and kind. "
         "If a request is completely incomprehensible, just a random string of letters with no meaning, "
         "or logically impossible to answer, reply EXACTLY with the word 'FAILSAFE_TRIGGER'."
     )
